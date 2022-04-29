79d945976f8c84a02c113220208425f4461afe13
Ivan Solodilov <optim000000@gmail.com>
2022-04-29 13:33:21 +0700

    Update freertos for linux build

--- build.sh
+++ build.sh
@@ -10,5 +10,5 @@ cd px4 && cmake ../.. -DTYPE=PX4 -DCMAKE_BUILD_TYPE=Debug-target
 make all
 
 # Posix build
-cd ../posix && cmake ../.. -DTYPE=Posix -DCMAKE_BUILD_TYPE=Debug-target
+cd ../posix && cmake ../.. -DTYPE=Linux -DCMAKE_BUILD_TYPE=Debug-target
 make all
