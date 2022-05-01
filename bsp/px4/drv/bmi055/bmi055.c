01108fecb3e8d3aba9e6f9f0f625e90b5f7a8347
Ivan Solodilov <optim000000@gmail.com>
2022-05-01 16:49:03 +0700

    Add prototypes of drv functions for bmi055

--- bsp/px4/drv/bmi055/bmi055.c
+++ bsp/px4/drv/bmi055/bmi055.c
@@ -15,6 +15,21 @@ enum bmi055_state_t {
 };
 enum bmi055_state_t bmi055_state;
 
+uint8_t BMI055_ReadReg(uint8_t reg);
+void BMI055_WriteReg(uint8_t reg, uint8_t value);
+void BMI055_SetClearReg(uint8_t reg, uint8_t setbits, uint8_t clearbits);
+int BMI055_Configure();
+void BMI055_AccelConfigure();
+void BMI055_GyroConfigure();
+int BMI055_Probe();
+void BMI055_Statistics();
+void BMI055_FIFOCount();
+int BMI055_FIFORead();
+void BMI055_FIFOReset();
+void BMI055_AccelProcess();
+void BMI055_GyroProcess();
+void BMI055_TempProcess();
+
 static gpio_cfg_t bmi055_gyro_cs = GPIO_SPI1_CS3;
 static gpio_cfg_t bmi055_accel_cs = GPIO_SPI1_CS4;
 static exti_cfg_t bmi055_gyro_drdy = EXTI_SPI1_DRDY3;
@@ -44,6 +59,29 @@ int BMI055_Init() {
     return rv;
 }
 
+void BMI055_AccelChipSelection() {
+    GPIO_Reset(&bmi055_accel_cs);
+}
+
+void BMI055_GyroChipSelection() {
+    GPIO_Reset(&bmi055_gyro_cs);
+}
+
+void BMI055_AccelChipDeselection() {
+    GPIO_Set(&bmi055_accel_cs);
+}
+
+void BMI055_GyroChipDeselection() {
+    GPIO_Set(&bmi055_accel_cs);
+}
+
 void BMI055_Run() {
+    switch(bmi055_state) {
+
+    case BMI055_RESET:
+    case BMI055_RESET_WAIT:
+    case BMI055_CONF:
+    case BMI055_FIFO_READ:
+        break;
 
 }
