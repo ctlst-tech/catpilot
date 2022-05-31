e58355a67ffa3cc21556a074c1d8122b9460cd2a
Ivan Solodilov <optim000000@gmail.com>
2022-05-31 12:58:32 +0700

    Dance with memory - Increase minimal stack size - Change stack memory location

--- bsp/px4/mcu/stm32f765ii/core/syscalls.c
+++ bsp/px4/mcu/stm32f765ii/core/syscalls.c
@@ -9,23 +9,23 @@ typedef	char *	caddr_t;
 
 caddr_t _sbrk(int incr)
 {
-  static char *heap_end;
-  char *prev_heap_end;
+    static char *heap_end;
+    char *prev_heap_end;
 
-  if (heap_end == 0) {
-    heap_end = &_end;
-  }
+    if (heap_end == 0) {
+        heap_end = &_end;
+    }
 
-  prev_heap_end = heap_end;
+    prev_heap_end = heap_end;
 
-  char *heap_desir = heap_end + incr;
-  char *heap_max = (char*)&_HeapLimit;
+    char *heap_desir = heap_end + incr;
+    char *heap_max = (char*)&_HeapLimit;
 
-  if (heap_desir >  heap_max) {
-    errno = ENOMEM;
-    return ((void*)-1);
-  }
-  heap_end += incr;
+    if (heap_desir >  heap_max) {
+        errno = ENOMEM;
+        return ((void*)-1);
+    }
+    heap_end += incr;
 
-  return (caddr_t) prev_heap_end;
+    return (caddr_t) prev_heap_end;
 }
--- os/freertos/FreeRTOSConfig.h
+++ os/freertos/FreeRTOSConfig.h
@@ -7,8 +7,8 @@
 #define configCPU_CLOCK_HZ			            ( ( unsigned long ) 216000000 )
 #define configTICK_RATE_HZ			            ( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES                    5
-#define configMINIMAL_STACK_SIZE                128 * 4
-#define configMAX_TASK_NAME_LEN                 32
+#define configMINIMAL_STACK_SIZE                1024
+#define configMAX_TASK_NAME_LEN                 64
 #define configUSE_16_BIT_TICKS                  0
 #define configIDLE_SHOULD_YIELD                 1
 #define configUSE_TASK_NOTIFICATIONS            1
@@ -29,7 +29,7 @@
 /* Memory allocation related definitions. */
 #define configSUPPORT_STATIC_ALLOCATION         1
 #define configSUPPORT_DYNAMIC_ALLOCATION        1
-#define configTOTAL_HEAP_SIZE                   ( size_t ) ( 300 * 1000 )
+#define configTOTAL_HEAP_SIZE                   ( size_t ) ( 300 * 1000 ) // Unuseless for heap3
 #define configAPPLICATION_ALLOCATED_HEAP        0 //change
 
 /* Hook function related definitions. */
