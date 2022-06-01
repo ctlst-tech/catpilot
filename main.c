42830d8ab60f7e4ebadd952195a6b74c3df571f7
Ivan Solodilov <optim000000@gmail.com>
2022-06-02 00:23:12 +0700

    Refact ringbuf

--- main.c
+++ main.c
@@ -18,8 +18,8 @@
 #include "mag.h"
 #include "logger.h"
 
-#define LOG_STDOUT_ENABLE 0
-#define ECHO_ENABLE 0
+#define LOG_STDOUT_ENABLE 1
+#define ECHO_ENABLE 1
 
 void main_thread(void *param);
 void *ctlst(void *param);
