a83edd38145faf7d83f5fe24e41d348be033c3da
Ivan Solodilov <optim000000@gmail.com>
2022-09-10 00:16:36 +0700

    Update registers header for ICM20948 (GYRO and ACCEL)

--- drv/icm20948/icm20948_reg.h
+++ drv/icm20948/icm20948_reg.h
@@ -5,98 +5,134 @@
 #define type_t static const uint8_t
 
 // Register addresses
-type_t XG_OFFS_TC_H         = 0x04;
-type_t XG_OFFS_TC_L         = 0x05;
-type_t YG_OFFS_TC_H         = 0x07;
-type_t YG_OFFS_TC_L         = 0x08;
-type_t ZG_OFFS_TC_H         = 0x0A;
-type_t ZG_OFFS_TC_L         = 0x0B;
-type_t CONFIG               = 0x1A;
-type_t GYRO_CONFIG          = 0x1B;
-type_t ACCEL_CONFIG         = 0x1C;
-type_t ACCEL_CONFIG2        = 0x1D;
-type_t FIFO_EN              = 0x23;
-type_t INT_PIN_CFG          = 0x37;
-type_t INT_ENABLE           = 0x38;
-type_t TEMP_OUT_H           = 0x41;
-type_t TEMP_OUT_L           = 0x42;
-type_t FIFO_WM_TH1          = 0x60;
-type_t FIFO_WM_TH2          = 0x61;
-type_t SIGNAL_PATH_RESET    = 0x68;
-type_t USER_CTRL            = 0x6A;
-type_t PWR_MGMT_1           = 0x6B;
-type_t I2C_IF               = 0x70;
-type_t FIFO_COUNTH          = 0x72;
-type_t FIFO_COUNTL          = 0x73;
-type_t FIFO_R_W             = 0x74;
-type_t WHO_AM_I             = 0x75;
-type_t XA_OFFSET_H          = 0x77;
-type_t XA_OFFSET_L          = 0x78;
-type_t YA_OFFSET_H          = 0x7A;
-type_t YA_OFFSET_L          = 0x7B;
-type_t ZA_OFFSET_H          = 0x7D;
-type_t ZA_OFFSET_L          = 0x7E;
+// BANK 1
+type_t WHO_AM_I             = 0x00;
+type_t USER_CTRL            = 0x03;
+type_t PWR_MGMT_1           = 0x06;
+type_t INT_PIN_CFG          = 0x0F;
+type_t INT_ENABLE_1         = 0x11;
+type_t I2C_MST_STATUS       = 0x17;
+type_t TEMP_OUT_H           = 0x39;
+type_t TEMP_OUT_L           = 0x3A;
+type_t EXT_SLV_SENS_DATA_00 = 0x3B;
+type_t EXT_SLV_SENS_DATA_23 = 0x52;
+type_t FIFO_EN_2            = 0x67;
+type_t FIFO_RST             = 0x68;
+type_t FIFO_MODE            = 0x69;
+type_t FIFO_COUNTH          = 0x70;
+type_t FIFO_COUNTL          = 0x71;
+type_t FIFO_R_W             = 0x72;
+type_t FIFO_CFG             = 0x76;
+type_t REG_BANK_SEL         = 0x7F;
 
-// CONFIG
-type_t FIFO_MODE            = BIT6;
-type_t DLPF_CFG_8KHZ        = 7;
-type_t DLPF_CFG_1KHZ        = 1;
+// BANK 2
+type_t GYRO_SMPLRT_DIV      = 0x00;
+type_t GYRO_CONFIG_1        = 0x01;
+type_t ACCEL_SMPLRT_DIV_2   = 0x11;
+type_t ACCEL_CONFIG         = 0x14;
 
-// GYRO_CONFIG
-type_t FS_SEL_250_DPS       = 0x00;
-type_t FS_SEL_500_DPS       = BIT3;
-type_t FS_SEL_1000_DPS      = BIT4;
-type_t FS_SEL_2000_DPS      = BIT4 | BIT3;
-type_t FCHOICE_B_8KHZ       = BIT1 | BIT0;
+// BANK 3
+type_t I2C_MST_CTRL         = 0x01;
+type_t I2C_MST_DELAY_CTRL   = 0x02;
+type_t I2C_SLV0_ADDR        = 0x03;
+type_t I2C_SLV0_REG         = 0x04;
+type_t I2C_SLV0_CTRL        = 0x05;
+type_t I2C_SLV0_DO          = 0x06;
+type_t I2C_SLV4_CTRL        = 0x15;
 
-// ACCEL_CONFIG
-type_t ACCEL_FS_SEL_2G      = 0x00;
-type_t ACCEL_FS_SEL_4G      = BIT3;
-type_t ACCEL_FS_SEL_8G      = BIT4;
-type_t ACCEL_FS_SEL_16G     = BIT4 | BIT3;
-
-// ACCEL_CONFIG2
-type_t ACCEL_FCHOICE_B      = BIT3;
-type_t A_DLPF_CFG_1KHZ      = 7;
-
-// FIFO_EN
-type_t GYRO_FIFO_EN         = BIT4;
-type_t ACCEL_FIFO_EN        = BIT3;
-
-// INT_PIN_CFG
-type_t INT_LEVEL            = BIT7;
-type_t INT_OPEN             = BIT6;
-type_t LATCH_INT_EN         = BIT5;
-type_t INT_RD_CLEAR         = BIT4;
-
-// INT_ENABLE
-type_t DATA_RDY_INT_EN      = BIT0;
-
-// SIGNAL_PATH_RESET
-type_t ACCEL_RST            = BIT1;
-type_t TEMP_RST             = BIT0;
-
-// USR_CTRL
-type_t USR_CTRL_FIFO_EN     = BIT6;
-type_t USR_CTRL_FIFO_RST    = BIT2;
-type_t SIG_COND_RST         = BIT0;
+// BANK 1 Registers
+// USER_CTRL
+type_t DMP_EN               = BIT7;
+type_t FIFO_EN              = BIT6;
+type_t I2C_MST_EN           = BIT5;
+type_t I2C_IF_DIS           = BIT4;
+type_t DMP_RST              = BIT3;
+type_t SRAM_RST             = BIT2;
+type_t I2C_MST_RST          = BIT1;
 
 // PWR_MGMT_1
 type_t DEVICE_RESET         = BIT7;
 type_t SLEEP                = BIT6;
+type_t CLKSEL_2             = BIT2;
+type_t CLKSEL_1             = BIT1;
 type_t CLKSEL_0             = BIT0;
 
-// I2C_IF_BIT
-type_t I2C_IF_DIS           = BIT6;
+// INT_PIN_CFG
+type_t INT1_ACTL            = BIT7;
+type_t BYPASS_EN            = BIT1;
 
-// Register values
-type_t WHOAMI               = 0x12;
+// INT ENABLE
+type_t RAW_DATA_0_RDY_EN    = BIT0;
 
-// Temp settings
-static const float TEMP_SENS     = 326.8f;
-static const float TEMP_OFFSET   = 25.f;
-static const float TEMP_SENS_MIN = -40.f;
-static const float TEMP_SENS_MAX = 85.f;
+// FIFO_EN_2
+type_t ACCEL_FIFO_EN        = BIT4;
+type_t GYRO_Z_FIFO_EN       = BIT3;
+type_t GYRO_Y_FIFO_EN       = BIT2;
+type_t GYRO_X_FIFO_EN       = BIT1;
+type_t TEMP_FIFO_EN         = BIT0;
+
+// FIFO_RST
+type_t FIFO_RESET           = BIT4 | BIT3 | BIT2 | BIT1 | BIT0;
+
+// FIFO_MODE
+type_t SNAPSHOT             = BIT0;
+
+// FIFO_CFG
+type_t FIFO_CFG_BIT         = BIT0;
+
+type_t USER_BANK_0          = 0;         
+type_t USER_BANK_1          = BIT4;      
+type_t USER_BANK_2          = BIT5;      
+type_t USER_BANK_3          = BIT5 | BIT4;
+
+// BANK 2 Registers
+// 5:3 GYRO_DLPFCFG[2:0]
+type_t GYRO_DLPFCFG         = BIT5 | BIT4 | BIT3;
+
+// 2:1 GYRO_FS_SEL[1:0]
+type_t GYRO_FS_SEL_250_DPS  = 0;           // ±250 dps
+type_t GYRO_FS_SEL_500_DPS  = BIT1;        // ±500 dps
+type_t GYRO_FS_SEL_1000_DPS = BIT2;        // ±1000 dps
+type_t GYRO_FS_SEL_2000_DPS = BIT2 | BIT1; // ±2000 dps
+type_t GYRO_FCHOICE         = BIT0;        // 0 – Bypass gyro DLPF
+
+// 5:3 ACCEL_DLPFCFG[2:0]
+type_t ACCEL_DLPFCFG        = BIT5 | BIT4 | BIT3;
+
+// 2:1 ACCEL_FS_SEL[1:0]
+type_t ACCEL_FS_SEL_2G      = 0;           // 0b00: ±2g
+type_t ACCEL_FS_SEL_4G      = BIT1;        // 0b01: ±4g
+type_t ACCEL_FS_SEL_8G      = BIT2;        // 0b10: ±8g
+type_t ACCEL_FS_SEL_16G     = BIT2 | BIT1; // 0b11: ±16g
+type_t ACCEL_FCHOICE        = BIT0;        // 0: Bypass accel DLPF
+
+// BANK 3 Registers
+// I2C_MST_CTRL
+type_t I2C_MST_P_NSR        = BIT4;
+
+// I2C_MST_DELAY_CTRL
+type_t I2C_MST_CLK_400_kHz  = 7;
+
+// I2C_MST_DELAY_CTRL
+type_t I2C_SLVX_DLY_EN      = BIT4 | BIT3 | BIT2 | BIT1 | BIT0;
+
+// I2C_SLV0_ADDR
+type_t I2C_SLV0_RNW         = BIT7;
+type_t I2C_ID_0             = BIT6 | BIT5 | BIT4 | BIT3 | BIT2 | BIT1 | BIT0;
+
+// I2C_SLV0_CTRL
+type_t I2C_SLV0_EN          = BIT7;
+type_t I2C_SLV0_BYTE_SW     = BIT6;
+type_t I2C_SLV0_REG_DIS     = BIT5;
+type_t I2C_SLV0_LENG        = BIT3 | BIT2 | BIT1 | BIT0;
+
+// I2C_SLV4_CTRL
+type_t I2C_MST_DLY          = BIT4 | BIT3 | BIT2 | BIT1 | BIT0;
+
+// Data
+type_t WHOAMI               = 0xEA; 
+
+#define FIFO_SIZE 512
 
 // Register read/write flag
 type_t READ = 0x80;
@@ -110,8 +146,6 @@ typedef struct {
     uint8_t ACCEL_YOUT_L;
     uint8_t ACCEL_ZOUT_H;
     uint8_t ACCEL_ZOUT_L;
-    uint8_t TEMP_H;
-    uint8_t TEMP_L;
     uint8_t GYRO_XOUT_H;
     uint8_t GYRO_XOUT_L;
     uint8_t GYRO_YOUT_H;
@@ -120,7 +154,9 @@ typedef struct {
     uint8_t GYRO_ZOUT_L;
 } FIFO_t;
 
-#define SIZE_REG_CFG 24
+#define BANK1_SIZE_REG_CFG 6
+#define BANK2_SIZE_REG_CFG 2
+#define BANK3_SIZE_REG_CFG 3
 
 typedef struct {
     uint8_t reg;
@@ -128,31 +164,24 @@ typedef struct {
     uint8_t clearbits;
 } reg_cfg_t;
 
-static const reg_cfg_t reg_cfg[SIZE_REG_CFG] = {
-    {CONFIG,        FIFO_MODE | DLPF_CFG_1KHZ, 0},
-    {GYRO_CONFIG,   FS_SEL_2000_DPS, FCHOICE_B_8KHZ},
-    {ACCEL_CONFIG,  ACCEL_FS_SEL_16G, 0},
-    {ACCEL_CONFIG2, A_DLPF_CFG_1KHZ, ACCEL_FCHOICE_B},
-    {FIFO_EN,       GYRO_FIFO_EN | ACCEL_FIFO_EN, 0},
-    {INT_PIN_CFG,   LATCH_INT_EN | INT_RD_CLEAR, 0},
-    {INT_ENABLE,    DATA_RDY_INT_EN, 0},
-    {FIFO_WM_TH1,   0, 0},
-    {FIFO_WM_TH2,   0, 0},
-    {USER_CTRL,     USR_CTRL_FIFO_EN, 0},
-    {PWR_MGMT_1,    CLKSEL_0, SLEEP},
-    {I2C_IF,        I2C_IF_DIS, 0},
-    {XG_OFFS_TC_H,  0, 0},
-    {XG_OFFS_TC_L,  0, 0},
-    {YG_OFFS_TC_H,  0, 0},
-    {YG_OFFS_TC_L,  0, 0},
-    {ZG_OFFS_TC_H,  0, 0},
-    {ZG_OFFS_TC_L,  0, 0},
-    {XA_OFFSET_H,   0, 0},
-    {XA_OFFSET_L,   0, 0},
-    {YA_OFFSET_H,   0, 0},
-    {YA_OFFSET_L,   0, 0},
-    {ZA_OFFSET_H,   0, 0},
-    {ZA_OFFSET_L,   0, 0},
+static const reg_cfg_t bank_1_reg_cfg[BANK1_SIZE_REG_CFG] = {
+    {USER_CTRL,    FIFO_EN | I2C_MST_EN | I2C_IF_DIS, DMP_EN},
+    {PWR_MGMT_1,   CLKSEL_0, DEVICE_RESET | SLEEP},
+    {INT_PIN_CFG,  INT1_ACTL, 0},
+    {INT_ENABLE_1, RAW_DATA_0_RDY_EN, 0},
+    {FIFO_EN_2,    ACCEL_FIFO_EN | GYRO_Z_FIFO_EN | GYRO_Y_FIFO_EN | GYRO_X_FIFO_EN, TEMP_FIFO_EN},
+    {FIFO_MODE,    SNAPSHOT, 0},
+};
+
+static const reg_cfg_t bank_2_reg_cfg[BANK2_SIZE_REG_CFG] = {
+    {GYRO_CONFIG_1, GYRO_FS_SEL_2000_DPS, GYRO_FCHOICE},
+    {ACCEL_CONFIG,  ACCEL_FS_SEL_16G, ACCEL_FCHOICE},
+};
+
+static const reg_cfg_t bank_3_reg_cfg[BANK3_SIZE_REG_CFG] = {
+    {I2C_MST_CTRL,       0, 0},
+    {I2C_MST_DELAY_CTRL, 0, 0},
+    {I2C_SLV4_CTRL,      0, 0},
 };
 
 typedef struct {
