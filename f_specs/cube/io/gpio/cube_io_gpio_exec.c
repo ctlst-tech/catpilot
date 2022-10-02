7bf76913390961329a9d37ea2473852f6458f65f
Ivan Makarov <ivan_makarov@icloud.com>
2022-10-02 19:24:43 -0400

    Update Cube gpio f_spec function for actual generated parameters

--- f_specs/cube/io/gpio/cube_io_gpio_exec.c
+++ f_specs/cube/io/gpio/cube_io_gpio_exec.c
@@ -5,10 +5,9 @@ void cube_io_gpio_exec(const cube_io_gpio_inputs_t *i, cube_io_gpio_outputs_t *o
 {
     if(p->channel < 1 || p->channel > 6) return;
 
-    if(i->opt_in_actvtd_input & 0x1) {
+    if(i->optional_in_input_connected) {
         GPIO_SetState(&gpio_fmu_pwm[p->channel - 1], i->input);
     } else {
         GPIO_Toggle(&gpio_fmu_pwm[p->channel - 1]);
     }
 }
-
