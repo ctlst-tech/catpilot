d4b98c0d28aae1b002a8e18da41163bf7c525d33
Ivan Solodilov <optim000000@gmail.com>
2023-01-19 20:16:00 +0400

    Add osa stub for sys/stat.h

---
+++ os/freertos/osa/include/FreeRTOS_POSIX/sys/stat.h
@@ -0,0 +1,12 @@
+#ifndef _FREERTOS_POSIX_STAT_H_
+#define _FREERTOS_POSIX_STAT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ifndef _FREERTOS_POSIX_STAT_H_ */
