2f144a57d5fba6ff75e8e8d3477c8f6b4ad79619
Ivan Makarov <ivan_makarov@icloud.com>
2023-02-02 17:16:07 -0500

    Adjust cube/sensors/adc atomic function

--- atomics/cube/sensors/adc/declaration.py
+++ atomics/cube/sensors/adc/declaration.py
@@ -11,10 +11,34 @@ Function(
             title='ADC channel',
             value_type='core.type.u8',
             default=0,
-        )],
+            constraints=[
+                ThisValue() >= 0,
+                ThisValue() <= 5
+            ]
+        ),
+        Parameter(
+            name='scale',
+            title='Conversion scale factor',
+            value_type='core.type.f64',
+            default=1.0,
+            constraints=[
+                ThisValue() >= 0.0,
+            ]
+        ),
+        Parameter(
+            name='offset',
+            title='Conversion offset',
+            description='Conversion offset applied before factor scaling',
+            value_type='core.type.f64',
+            default=0.0,
+            constraints=[
+                ThisValue() >= 0.0,
+            ]
+        )
+    ],
     outputs=[
         Output(
-            name='o',
+            name='output',
             title='Current voltage',
             value_type='core.type.f64'
         ),
