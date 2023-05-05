17297e4bf1899d7e3289d581e7503bd88b8fcd09
Ivan Solodilov <optim000000@gmail.com>
2023-05-05 11:41:42 +0400

    Add pwm in declaration

---
+++ atomics/ctlst/io/pwm_in/ctlst_io_pwm_in_exec.c
@@ -0,0 +1,10 @@
+#include "ctlst_io_pwm_in.h"
+
+fspec_rv_t ctlst_io_pwm_in_pre_exec_init(const ctlst_io_pwm_in_params_t *p) {
+    return fspec_rv_ok;
+}
+
+void ctlst_io_pwm_in_exec(ctlst_io_pwm_in_outputs_t *o,
+                          const ctlst_io_pwm_in_params_t *p) {
+    return;
+}
---
+++ atomics/ctlst/io/pwm_in/declaration.py
@@ -0,0 +1,52 @@
+from fspeclib import *
+
+Function(
+    name='ctlst.io.pwm_in',
+    title=LocalizedString(
+        en='pwm'
+    ),
+    has_pre_exec_init_call=True,
+    custom_cmakefile=True,
+    parameters=[
+        Parameter(
+            name='channel',
+            title='PWM channel',
+            value_type='core.type.u32',
+            tunable=False,
+            default=0
+        ),
+        Parameter(
+            name='min',
+            title='Channel min PWM',
+            value_type='core.type.u32',
+            default=700,
+        ),
+        Parameter(
+            name='max',
+            title='Channel max PWM',
+            value_type='core.type.u32',
+            default=700,
+        ),
+        Parameter(
+            name='period',
+            title='PWM period us',
+            value_type='core.type.u32',
+            default=1000,
+        ),
+        Parameter(
+            name='bipolar',
+            title='Bipolar mode enable',
+            value_type='core.type.bool',
+            default='FALSE',
+        ),
+    ],
+    inputs=[],
+    outputs=[
+        Output(
+            name='output',
+            title='Output',
+            value_type='core.type.f64'
+        )],
+    state=[],
+    parameter_constraints=[]
+)
